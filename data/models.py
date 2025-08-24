3179850b1981378b53405e955032f82bc0135dae
hamedhp1375 <hamedparnian1375hp@gmail.com>
2025-08-24 11:16:10 +0330

    add tag

--- data/models.py
+++ data/models.py
@@ -34,6 +34,17 @@ class Category(models.Model):
         verbose_name = 'category'
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        db_table = 'Tag'
+        ordering = ['name']
+        verbose_name_plural = 'tags'
+        verbose_name = 'tag'
+
+
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
@@ -43,6 +54,8 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag,blank=True)
 
     class Meta:
         db_table = 'Post'
@@ -67,11 +80,13 @@ class User(AbstractUser):
         verbose_name_plural = 'users'
         verbose_name = 'user'
 
-class Comment (models.Model):
+
+class Comment(models.Model):
     text = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         db_table = 'Comment'
         ordering = ['created']
